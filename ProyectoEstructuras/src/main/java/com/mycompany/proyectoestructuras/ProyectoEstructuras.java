a50a95edc370de43b8077e17d0d5bb787e7e58d8
Gabo0323 <88918141+Gabo0323@users.noreply.github.com>
2023-09-20 20:20:14 -0600

    Add files via upload

---
+++ ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/ProyectoEstructuras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoestructuras;
+
+/**
+ *
+ * @author Dell
+ */
+public class ProyectoEstructuras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
