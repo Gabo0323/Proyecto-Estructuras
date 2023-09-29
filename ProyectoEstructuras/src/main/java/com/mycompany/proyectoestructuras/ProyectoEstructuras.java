8eaaf99fa170518575fc2c452e74a08f0bbd28c7
Gabo0323 <88918141+Gabo0323@users.noreply.github.com>
2023-09-29 16:27:28 -0600

    Modelo vista controlador
    Creación de paquetes y clases para aplicar el modelo vista controlador del programa

--- ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/ProyectoEstructuras.java
+++ ProyectoEstructuras/src/main/java/com/mycompany/proyectoestructuras/ProyectoEstructuras.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectoestructuras;
 public class ProyectoEstructuras {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hola");
     }
 }
